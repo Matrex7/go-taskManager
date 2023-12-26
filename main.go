baaa9152500b6b55b69f0630391525d4b21ae0a8
Matrex <omarmatrexyt@gmail.com>
2023-12-26 12:44:17 +0100

    adding task type and main function

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+type Task struct {
+	id int 
+	title string
+	description string 
+	startDate time.Time
+	finishDate time.Time
+}
+func main()  {
+	
+}
